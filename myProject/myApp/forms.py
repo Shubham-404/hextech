57d6ec3262349a47e8fba6ed1ab181206d5d4648
Shubham <shubh.shubhamkrsingh@gmail.com>
2024-12-13 17:11:45 +0530

    restored previous version

---
+++ myProject/myApp/forms.py
@@ -0,0 +1,13 @@
+# forms.py
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import CustomUser
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password1', 'password2']
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
