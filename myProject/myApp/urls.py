c1bf0bc4953f469bd6f93a7abed1809abc4f4bd9
Shubham <shubh.shubhamkrsingh@gmail.com>
2024-12-13 14:22:45 +0530

    frontend ready

---
+++ myProject/myApp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name = "home"),
+    path("insights/", views.insights, name = "insights"),
+    path("devices/", views.devices, name = "devices"),
+    path("join/", views.join, name = "join"),
+
+]
---
+++ myProject/myApp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+logged = True;
+def home(request) : 
+    if(logged):
+        return render(request, "myApp/home.html")
+    else:
+        return render(request, "myApp/join.html")
+        
+
+def insights(request):
+    return render(request, "myApp/insights.html")
+
+def devices(request):
+    return render(request, "myApp/devices.html")
+
+def join(request):
+    return render(request, "myApp/join.html")
